6dd8f089ea0496d40be0272ad63a6931bf3c6fbf
raghad-hakim <104999624+raghad-hakim@users.noreply.github.com>
2022-05-11 17:12:39 +0300

    Add files via upload

---
+++ 1-A.py
@@ -0,0 +1,6 @@
+graduated=['raghad','alia','sally','madian','ismaeel']
+student=input('enter student name to see result')
+if student in graduated:
+    print(student,"graduated",sep=" ***** ")
+else:
+    print(student,"not graduated",sep=" !!!! ")
